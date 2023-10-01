52ea9f6578d1f6cf7f2765b14e10acf809e61b82
abdullah137 <abdullahyahaya46@gmail.com>
2023-10-01 18:02:48 +0100

    feat: created the update vault dto

--- src/vault/dto/update-vault.dto.ts
+++ src/vault/dto/update-vault.dto.ts
@@ -2,12 +2,7 @@ import { IsNumber, IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateVaultDto {
   @IsNotEmpty()
-  @IsNumber()
-  @IsOptional()
-  userId: number;
-
-  @IsNotEmpty()
   @IsString()
   @IsOptional()
-  data: string;
+  data?: string;
 }
