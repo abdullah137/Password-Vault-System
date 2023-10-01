26f9c728021d9c8e3fab431d3465686ba405e3ad
abdullah137 <abdullahyahaya46@gmail.com>
2023-10-01 18:03:54 +0100

    refactor: added the vault logic to the controller

--- src/vault/vault.controller.ts
+++ src/vault/vault.controller.ts
@@ -4,13 +4,14 @@ import { UpdateVaultDto } from './dto/update-vault.dto';
 import { User } from '@prisma/client';
 import { GetUser } from '../user/decorator/get-user.decorator';
 import { JwtGuard } from '../user/guard';
-@Controller('vault')
+import { VaultLogic } from './logic/vault.logic';
 @UseGuards(JwtGuard)
+@Controller('vault')
 export class VaultController {
-  constructor(private readonly vaultService: VaultService) {}
+  constructor(private vaultLogic: VaultLogic) {}
 
   @Put()
-  async updateVault(@Body() paylod: UpdateVaultDto, @GetUser() user: User) {
-    console.log(user);
+  async updateVault(@Body() payload: UpdateVaultDto, @GetUser() user: User) {
+    return this.vaultLogic.updateVault(payload, user);
   }
 }
