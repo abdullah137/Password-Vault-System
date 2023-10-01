1ba5592a1aad462c7a8042d21667797aa74973bc
abdullah137 <abdullahyahaya46@gmail.com>
2023-10-01 18:01:38 +0100

    feat: added vault logic and user service as a provider

--- src/vault/vault.module.ts
+++ src/vault/vault.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VaultService } from './vault.service';
 import { VaultController } from './vault.controller';
+import { VaultLogic } from './logic/vault.logic';
+import { UserService } from 'src/user/user.service';
 
 @Module({
   controllers: [VaultController],
-  providers: [VaultService]
+  providers: [VaultService, UserService, VaultLogic],
 })
 export class VaultModule {}
